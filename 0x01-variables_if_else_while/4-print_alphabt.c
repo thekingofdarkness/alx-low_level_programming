c77fe02b1c3db8f048fb48bb89d18df7e52a43e3
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-16 15:01:53 -0700

    testing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point
+ * Description:
+ * it is the axe of thhis thing
+ * Retrun: 0 is exited prperly
+ */
+int main(void)
+{
+	char chars;
+	
+	for (chars = 'a'; chars <= 'z'; chars++)
+	{
+		if (chars == 'e' || chars == 'q')
+		{
+			chars++;
+			continue;
+		}
+		putchar(chars);
+	}
+}
