8133aead9ad151bb44b8877ad35936fa89b3ca85
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-16 16:04:14 -0700

    testing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,24 +7,21 @@
  */
 int main(void)
 {
-    int i, j;
+	int digit = 0;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
 
-            if (i != 9 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
-    putchar('\n');
+		++digit;
+	}
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
