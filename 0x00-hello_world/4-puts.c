59b8cd5183a93ea6ed1aa0c1897d02b5117df61d
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-15 18:15:47 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main  prints exactly Programming is like building a multilingual puzzl
  * return 0 if exited properly return non zero otherwise
  */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
