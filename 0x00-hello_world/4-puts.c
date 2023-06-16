07b53006228120053376f215d105b679883ff392
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-15 18:22:23 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- * This function prints the message "Programming is like building a multilingual puzzle"
+ * main - prints the message "Programming is like building a multilingual puzzle"
  * Return: Always 0 (indicating success)
 */
 int main(void)
