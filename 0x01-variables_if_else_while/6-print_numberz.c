e3625de2187362954a449e8872fab1f67263d817
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-16 15:14:48 -0700

    testing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Is the entry point for the program
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
