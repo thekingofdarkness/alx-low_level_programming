379752b453ac525987de02bd8b385721385b1f48
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-15 18:23:11 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the message "Programming is like building a multilingual puzzle"
+ * main - prints  "Programming is like building a multilingual puzzle"
  * Return: Always 0 (indicating success)
 */
 int main(void)
