209c74a47f00e7623a3d8a8cecd840aa9aa73fe7
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-15 18:25:12 -0700

    h

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the required message
+ * return 0 if works properly
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
