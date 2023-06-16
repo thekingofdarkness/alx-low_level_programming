43984c270be6cbf4f2ab4745683e9f8b52edc97a
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-16 14:43:26 -0700

    testing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The entry point for the program
+ * Description:
+ * this function is the entry point of this program
+ * Return: 0 if exited properly
+ */
+int main() 
+{
+	char alphabets
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	return 0;
+}
