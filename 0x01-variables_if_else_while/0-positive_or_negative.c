060232c30d013aa786c19da638dd3e1fe4d0274e
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-16 12:54:47 -0700

    commiting

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - is the start point of the program
+ * main - Is the start point of the program
  * Return: 0 if exited properly
   */
 int main(void)
@@ -18,8 +18,7 @@ int main(void)
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
-sh: 1: q: not found
-	else 
+	else
 	{
 		printf("%d is zero\n", n);
 	}
