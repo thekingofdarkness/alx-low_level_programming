ca7d88b13f25cbbeb0db12b787b66436aad8e71d
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-22 17:34:06 -0700

    auto commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * _isupper - checks for upper case
- * @c: the arg to be checked
- * Return: 0 for non upper and 1 for upper
+ * _isupper - it checks for uppercase
+ * @c: character to be checked
+ *Return: 0 for non-upper and 1 for upper
  */
-_isupper(c)
+int _isupper(int c)
 {
-	return (c>= 'A' && c <= 'Z');
-}
+	return (c >= 'A' && c <= 'Z');
+};
