be6b83983b989daef8eb70db5005ce83d1228f17
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-22 17:51:57 -0700

    auto commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers -prints from number 0 to 9
+ *
+ *Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+sh: 1: q: not found
