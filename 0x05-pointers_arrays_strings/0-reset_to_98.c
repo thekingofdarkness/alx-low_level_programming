9a29072338f1c76446722114e1aa6e453df31b48
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-26 16:40:28 -0700

    auto commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,13 +7,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;	
 }
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
