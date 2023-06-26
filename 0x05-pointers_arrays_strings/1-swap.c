b9158661d088c60e3f2ae0127befe3d245f5e04a
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-26 16:49:48 -0700

    auto commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * swap_int - swapes two variable by adress
+ * @a: the adress of the first var
+ * @b: the adress of the second var
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int holder = a;
+	*a = b;
+	*b = holder;
+}
