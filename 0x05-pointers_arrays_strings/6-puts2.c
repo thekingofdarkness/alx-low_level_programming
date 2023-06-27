5656938ad6c26ebaaebcac1ffbd111a789cfffdb
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-26 18:14:52 -0700

    auto commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,19 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int len, i;
 
-	while (str[i] != '\0')
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		putchar(str[i]);
-		i += 2;
+		len++;
 	}
 
-	putchar('\n');
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
