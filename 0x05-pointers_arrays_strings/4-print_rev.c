b60667fe9ac226c64bddc1a07efafceefa3f539e
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-26 17:47:52 -0700

    auto commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,13 +2,14 @@
 #include <string.h>
 /**
  * print_rev - prints a string, in reverse
- * @s: the string 
+ * @s: the string
  * Retrun: void
  */
 void print_rev(char *s)
 {
 	int len = strlen(s);
 	int i;
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		putchar(s[i]);
