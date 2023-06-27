d2019f7025e577327770133f0b0b3d6f738af2c6
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-06-26 18:01:10 -0700

    auto commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,10 @@ void rev_string(char *s)
 {
 	int end = strlen(s) - 1;
 	int start = 0;
+	char *t = *s;
 	while (start < end)
 	{
-		s[start] = s[end];
+		s[start] = t[end];
 		start++;
 		end--;
 	}
