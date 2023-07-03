e30395e838e354366e8103553f6747bc0c3021d2
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-03 13:48:17 -0700

    auto commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,5 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return memcpy(dest, src, n);
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
