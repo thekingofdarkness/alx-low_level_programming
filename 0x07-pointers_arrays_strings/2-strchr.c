1bf48a1be9ce1f13fcd0d7f86e9db4caccac5a49
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-03 14:02:58 -0700

    auto commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strchr -  locates a character in a string.
+ * @s: the string
+ * @c: the character
+ * Return: pointer to c if found else return null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '/0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return (NULL);
+		}
+		s++;
+	}
+}
