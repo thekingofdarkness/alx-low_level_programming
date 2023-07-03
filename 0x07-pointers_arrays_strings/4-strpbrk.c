b77ad0c9f0055d7d0af66467ec36df6ff00e511a
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-03 14:42:29 -0700

    auto commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,11 +11,11 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; *s != '\0'; i++) 
+	for(i = 0; *s != '\0'; i++) 
 	{
-		for (j = 0; *accept != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == *accept)
+			if (*s == accept[j])
 			{
 				return (s);
 			}
