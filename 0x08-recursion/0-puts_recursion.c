29ce8de67ddfaa10c8fb9cd50a3b01cdefc8fca4
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-05 07:10:15 -0700

    auto commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
+	if (*s == '\0')
 		{
 			putchar('\n');
 			return;
 		}
-        else
+	else
 		{
-                putchar(*s);
-                _puts_recursion(s+1);
+			putchar(*s);
+			_puts_recursion(s+1);
 		}
 }
