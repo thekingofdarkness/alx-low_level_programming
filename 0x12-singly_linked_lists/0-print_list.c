2666276d39c78de3f368ea35c172a9db04c926b2
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-28 05:32:32 -0700

    fixing first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0; // Variable to store the number of nodes printed.
+	size_t node_count = 0;
 
-    // Traverse the linked list using a while loop.
-    while (h)
-    {
-        if (!h->str) // If the string pointer is NULL.
-        {
-            printf("[0] (nil)\n"); // Print "[0] (nil)" to indicate an empty string.
-        }
-        else
-        {
-            printf("[%u] %s\n", h->len, h->str); // Print the string's length and content.
-        }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next; // Move to the next node in the linked list.
-        node_count++; // Increment the node count for each node printed.
-    }
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count; // Return the total number of nodes printed.
+	return (node_count);
 }
