6200a0e220f78dc15035b541fe2f9ba53e9fe858
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-07-28 05:52:04 -0700

    second task commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the list_t list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
