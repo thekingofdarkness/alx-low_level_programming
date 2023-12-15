1ed17d680e754d56b0e9977993c1aa319f9c73f0
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2023-12-15 12:18:57 -0800

    auto

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy -  copies memory area.
+ * @n: number of bytes to be copied
+ * @src: source of mem area
+ * @dest: destination mem area
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _puts -  prints a string followed by new line
+ * @str: the str to be printed with a new line
+ * Retrun: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
